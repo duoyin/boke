673d61762abbde38ab917f0ab655c83a81163631
张明 <ming@4ay.cn>
2022-06-04 17:03:05 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
 {
@@ -15,5 +16,19 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'name' => 'required|unique:users|min:3|max:50',
+                'email' => 'required|email|unique:users|max:255',
+                'password' => 'required|confirmed|min:6|max:255'
+            ]);
+        } catch (ValidationException $e) {
+        }
+
+        return;
     }
 }
