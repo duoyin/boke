04e64db58330e4aa4fe36afb90237b5861f613a1
张明 <ming@4ay.cn>
2022-06-04 19:46:04 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,11 +21,18 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|unique:users|min:3|max:50',
+            'name' => 'required|unique:users|min:2|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6|max:255'
         ]);
 
-        return;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        session()->flash('success', '宝贝，我准备好了，搞快点，搞快点！！！😍');
+        return redirect()->route('users.show', $user);
     }
 }
